e1e7bf8cb179578d67f90a93528566d7ecaee27e
Alex <alexcastrovarela@gmail.com>
2024-10-25 15:45:37 +0200

    up

--- src/support.py
+++ src/support.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def obtener_sopa(url):
     # Hacer una solicitud HTTP a la URL
@@ -14,4 +15,25 @@ def obtener_sopa(url):
         print(f"Error al acceder a {url}: Código de estado {response.status_code}")
         return None
 
+def sacar_tabla(url):
 
+    # Extraer el título del <h2>
+    title = url.find('h2').text.strip('Tabla de precios por día para')
+
+    # Localizar la tabla por su clase
+    table = url.find('table', {'class': 'table table-striped table-responsive text-center'})
+
+    # Extraer datos de la tabla
+    data = []
+    for row in table.find_all('tr')[1:]:  # Omitir la fila de encabezados
+        cols = row.find_all('td')
+        day = cols[0].text.strip()
+        price = cols[1].text.strip()
+        variation = cols[2].text.strip()
+        data.append([day, price, variation])
+
+    # Convertir en DataFrame de pandas
+    df = pd.DataFrame(data, columns=["Día", "Precio (€)", "Variación"])
+
+    # Agregar el título como una columna adicional
+    df['Producto'] = title
